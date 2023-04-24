eb34baab97c8b0257dc5fda69f402898726e7d90
Gabbylf <gfrint@ucsc.edu>
2023-04-23 20:10:04 -0700

    Bleh

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Roch Paper Scissors: The Computer Game
+//An digital look at the beloved hand game to help solve every two people's arguements..... Before it gets turned into best 2/3 then to best 4/7... and so on...
+//Gabriella Frint <gfrint@ucsc.edu>
+//April 2023
+//Creating a Rock, Paper, Scissors Game
+//Create variables for:
+//Rock
+//Paper
+//Scissors
+//Print text: “Rock, Paper, Scissors…”
+//Prompt user input
+//Computer chooses variable at random
+//Print text: “Shoot!”
+//Print text: “User: (user’s variable), Computer: (computer’s variable)”
+//Compares variable:
+//Rock beats scissor
+//Paper beats rock
+//Scissor beats paper
+//Print text: “Result:  (User/computer) Wins!” 
+//Or: Print text: “Result: Draw!”
+//Print text: “Continue?” and prompt user input
+//If user inputs “yes”: restart from 2
+//If the user inputs “no”: print text: “Thanks for playing!”
