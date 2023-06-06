dfc0332998e01444153d53cac12ae5425189087f
Gabbylf <gfrint@ucsc.edu>
2023-06-05 17:29:30 -0700

    well that was stressful

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,7 +3,7 @@
 //Gabriella Frint <gfrint@ucsc.edu>
 //June 2023
 
-function getComic(num) {
+function comic(num) {
     if (typeof num === 'undefined') {
       numStr = "";
     } else {
@@ -16,24 +16,24 @@ $.ajax({
 })
 
 .done(function(data) {
-    var imageUrl = data.img;
+    var img = data.img;
     var title = data.title;
     var alt = data.alt;
     var comicNum = data.num;
     var html = `<div id="imageblock">
         <h2>${title}</h2>
-        <img src="${imageUrl}" title="${alt}"><br>
+        <img src="${img}" title="${alt}"><br>
         <button id="prev">Previous</button><button id="next">Next</button>
       </div>`
     $("#output").html(html);
     $("#prev").click(function(){
-      getComic(comicNum - 1);
+      comic(comicNum - 1);
     });
     $("#next").click(function(){
-      getComic(comicNum + 1);
+      comic(comicNum + 1);
     });
 })
 .fail(function(){
 })
 }
-getComic();
+comic();
